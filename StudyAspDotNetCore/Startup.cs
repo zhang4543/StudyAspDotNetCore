c253399431cd009cfd7d078c6a398ba839f25f53
zsk <zhang4543@gmail.com>
2017-05-03 21:46:51 +0800

    异常处理

--- StudyAspDotNetCore/Startup.cs
+++ StudyAspDotNetCore/Startup.cs
@@ -62,7 +62,7 @@ namespace StudyAspDotNetCore
             //ip中间件
             app.UseRequestIP();
 
-           
+
 
             //启用默认页
             //UseDefaultFiles 必须在 UseStaticFiles 之前调用。UseDefaultFiles 只是重写了 URL，而不是真的提供了这样一个文件。你必须开启静态文件中间件（UseStaticFiles）来提供这个文件。
@@ -78,29 +78,50 @@ namespace StudyAspDotNetCore
             //MIME映射修改
             var provider = new FileExtensionContentTypeProvider();
             provider.Mappings[".cs"] = "text/plain";//wwwroot下的.cs可以访问了
-            app.UseStaticFiles(new StaticFileOptions() {
-                FileProvider=new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(),@"wwwroot")),
-                ContentTypeProvider=provider
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot")),
+                ContentTypeProvider = provider
             });
 
 
             //开放wwwroot以外的文件夹,访问静态文件,但是访问不了里面的.cs文件.?
-            app.UseStaticFiles(new StaticFileOptions (){
-                FileProvider=new PhysicalFileProvider(
-                    Path.Combine(Directory.GetCurrentDirectory(),@"MyStaticFiles")),
-                RequestPath=new PathString("/StaticFiles"),
-                ServeUnknownFileTypes=true,//开启不能识别的文件访问,不加默认404,这样指定文件夹下.cs文件可以作为文本浏览了
-                DefaultContentType= "text/plain"
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(Directory.GetCurrentDirectory(), @"MyStaticFiles")),
+                RequestPath = new PathString("/StaticFiles"),
+                ServeUnknownFileTypes = true,//开启不能识别的文件访问,不加默认404,这样指定文件夹下.cs文件可以作为文本浏览了
+                DefaultContentType = "text/plain"
 
             });
 
             //允许直接浏览目录
-            app.UseDirectoryBrowser(new DirectoryBrowserOptions() {
-                FileProvider=new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(),@"MyStaticFiles")),
-                RequestPath=new PathString("/StaticFiles")
+            app.UseDirectoryBrowser(new DirectoryBrowserOptions()
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"MyStaticFiles")),
+                RequestPath = new PathString("/StaticFiles")
             });
 
-           
+            #region snippet_AppRun
+            app.Run(async (context) =>
+            {
+                if (context.Request.Query.ContainsKey("throw"))
+                {
+                    throw new Exception("Exception triggered!");
+                }
+                var builder = new StringBuilder();
+                builder.AppendLine("<html><body>Hello World!");
+                builder.AppendLine("<ul>");
+                builder.AppendLine("<li><a href=\"/?throw=true\">Throw Exception</a></li>");
+                builder.AppendLine("<li><a href=\"/missingpage\">Missing Page</a></li>");
+                builder.AppendLine("</ul>");
+                builder.AppendLine("</body></html>");
+
+                context.Response.ContentType = "text/html";
+                await context.Response.WriteAsync(builder.ToString());
+            });
+            #endregion
 
             app.UseMvc(routes =>
             {
@@ -108,6 +129,8 @@ namespace StudyAspDotNetCore
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+
         }
     }
 }
